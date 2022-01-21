7923def9db47b996e85e758e4e99ee57afcb319e
Jacob Zirbel <jacobzirbel@gmail.com>
2022-01-21 00:03:28 -0600

    working websockets

--- app/public/script.js
+++ app/public/script.js
@@ -1,48 +1,110 @@
+
 let app;
-let player;
-window.onload = () => {
+let myself = { id: null };
+let players = {};
+const socket = new WebSocket('ws://localhost:3000');
 
-    fetch("/api/move").then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data)
-    });
-    app = new PIXI.Application(
-        {
-            width: 800,
-            height: 600,
-            backgroundColor: 0xAAAAAA
-        }
-    );
 
-    document.body.appendChild(app.view);
-    player = new PIXI.Sprite.from("images/red-player.png");
-    player.anchor.set(0.5);
-    player.x = app.view.width / randomNumber(4);
-    player.y = app.view.height / randomNumber(4);
+app = new PIXI.Application(
+    {
+        width: 800,
+        height: 600,
+        backgroundColor: 0xAAAAAA
+    }
+);
 
-    app.stage.addChild(player);
-}
+document.body.appendChild(app.view);
 
-document.addEventListener('keyup', (e) => {
-    const a = 10;
-    switch (e.key) {
-        case 'ArrowUp':
-            player.y -= a;
-            break;
-        case 'ArrowDown':
-            player.y += a;
-            break;
-        case 'ArrowRight':
-            player.x += a;
-            break;
-        case 'ArrowLeft':
-            player.x -= a;
-            break;
+
+socket.addEventListener('open', (event) => {
+    console.log('connected to ws server')
+});
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    console.log('new message from server', data)
+    if (data.type === 'newPlayer') {
+        console.log('new player')
+        const sprite = new PIXI.Sprite.from("images/blue-player.png");
+        sprite.anchor.set(0.5);
+        sprite.x = app.view.width / 2;
+        sprite.y = app.view.height / 2;
+        app.stage.addChild(sprite);
+        players[data.id] = { sprite, x: null, y: null };
+    } else if (data.type === 'welcome') {
+        const mysprite = new PIXI.Sprite.from("images/red-player.png");
+        mysprite.anchor.set(0.5);
+        mysprite.x = app.view.width / 2;
+        mysprite.y = app.view.height / 2;
+        myself.sprite = mysprite;
+        myself.id = data.id;
+        players[data.id] = myself;
+        app.stage.addChild(mysprite);
+        console.log('data.currentPlayers', data.currentPlayers)
+        data.currentPlayers.forEach(player => {
+            if(player.id == myself.id) return;
+            const sprite = new PIXI.Sprite.from("images/blue-player.png");
+            sprite.anchor.set(0.5);
+            sprite.x = +player.x;
+            sprite.y = +player.y;
+            const newPlayer = {};
+            newPlayer.sprite = sprite;
+            newPlayer.id = player.id;
+            players[player.id] = newPlayer;
+            app.stage.addChild(sprite);
+        });
+        const message = { id: myself.id, type: "move", position: { x: myself.sprite.x, y: myself.sprite.y } };
+        sendMessage(message);
+    } else if(data.type === 'playerMove') {
+        players[data.id].sprite.x = +data.x;
+        players[data.id].sprite.y = +data.y;
     }
 });
+
+const sendMessage = (data) => {
+    socket.send(JSON.stringify({ id: self.id, data: data }));
+}
+
+
+// fetch("/api/move").then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data)
+// });
+
+
+
+
+document.addEventListener('keyup', (e) => {
+    moveSprite(myself.sprite, e.key)
+    const message = { id: myself.id, type: "move", position: { x: myself.sprite.x, y: myself.sprite.y } };
+    sendMessage(message);
+});
+
+function newPlayer() {
+
+}
 
 function randomNumber(max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max)) + 1;
+}
+
+
+function moveSprite(sprite, direction) {
+    const a = 10;
+
+    switch (direction) {
+        case 'ArrowUp':
+            sprite.y -= a;
+            break;
+        case 'ArrowDown':
+            sprite.y += a;
+            break;
+        case 'ArrowRight':
+            sprite.x += a;
+            break;
+        case 'ArrowLeft':
+            sprite.x -= a;
+            break;
+    }
 }
--- server.js
+++ server.js
@@ -2,12 +2,55 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
+const server = require('http').createServer(app);
+
 const apiRoutes = require("./app/routes/api-routes");
 const htmlRoutes = require("./app/routes/html-routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "app/public")));
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server })
+
+let currentId = 0;
+let players = {};
+
+wss.on('connection', ws => {
+    console.log('new client connected');
+    currentId++;
+    players[currentId] = { x: null, y: null };
+    const currentPlayers = Object.keys(players).map(key => {
+        const item = players[key];
+        return {id: key, ...item};
+    })
+    ws.send(s({ type: 'welcome', id: currentId.toString(), currentPlayers }));
+    sendToAll(ws, s({ type: 'newPlayer', id: currentId.toString() }))
+
+    ws.on('message', message => {
+        console.log('recieved ' + message);
+        message = JSON.parse(message);
+        console.log('message.data.type', message.data.type)
+        if(message.data.type === 'move') {
+            players[message.data.id] = message.data.position;
+            console.log(JSON.stringify(players))
+            sendToAll(ws, s({
+                type: 'playerMove',
+                id: message.data.id.toString(),
+                x: message.data.position.x,
+                y: message.data.position.y
+            }));
+        }
+
+    });
+
+    ws.on('close', data => {
+        console.log('CLOSE', data)
+    })
+
+})
+
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
@@ -16,6 +59,22 @@ app.get("*", function (req, res) {
 	res.redirect("/home");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log("on " + PORT);
 });
+
+function s(json) {
+    return JSON.stringify(json);
+}
+
+function p(json) {
+    return JSON.parse(json);
+}
+
+function sendToAll(ws, message) {
+    wss.clients.forEach(client => {
+        if(client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    })
+}
